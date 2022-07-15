d6e8d66a91cbf25be6553baa88d4a13f6f381ba4
Software Dev 010 <101187139+softwaredev010@users.noreply.github.com>
2022-07-15 10:41:28 +0530

    Project initiation

---
+++ src/main/java/com/imdb/service/ImdbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.imdb.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImdbServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/imdb/service/ImdbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdb.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
