7dd398eb161edb3d2dceb1769dd5c94f67fac4be
Chaminda Jayawardana <101187139+softwaredev010@users.noreply.github.com>
2022-07-15 10:48:19 +0530

    call load data when application start

--- src/main/java/com/imdb/service/ImdbServiceApplication.java
+++ src/main/java/com/imdb/service/ImdbServiceApplication.java
@@ -8,6 +8,16 @@ public class ImdbServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ImdbServiceApplication.class, args);
+
+    //Load Data
+    loadData();
+  }
+
+  /**
+   * Loading IMDB datasets into the in-memory H2 database
+   */
+  private static void loadData(){
+
   }
 
 }
